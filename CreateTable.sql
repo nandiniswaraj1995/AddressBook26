caa09459cc14f92398a3eae3a717edcdcbede412
unknown <nandiniswaraj13@gmail.com>
2021-04-17 11:17:42 +0530

    [Nandini swaraj] Added Create table

---
+++ CreateTable.sql
@@ -0,0 +1,12 @@
+use addressBookService26DB;
+create table AddressBook
+(
+fisrt_name varchar(40),
+last_name varchar(40),
+address varchar(40),
+city varchar(40),
+state varchar(30),
+zip varchar(10),
+phone_number varchar(15),
+email varchar(50)
+);
