78efa98556d7c61cce1792d01220f47a481b8bdb
unknown <nandiniswaraj13@gmail.com>
2021-04-17 12:48:54 +0530

    [Nandini swaraj] insert same data to different book type'

---
+++ InsertSameDataToDifferentBookType.sql
@@ -0,0 +1,26 @@
+insert into AddressBook values
+(
+   'dddd',
+   'qqqq',
+   'BTM',
+   'Bangalore',
+   'Karnataka',
+   '560076',
+   '9876543210',
+   'ddd@gmail.com',
+   'mybook4',
+   'friend'
+   ),
+  
+  (
+   'dddd',
+   'qqqq',
+   'BTM',
+   'Bangalore',
+   'Karnataka',
+   '560076',
+   '9876543210',
+   'ddd@gmail.com',
+   'mybook4',
+   'family'
+   )
